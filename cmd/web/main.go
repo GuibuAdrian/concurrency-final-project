3e6626afd0b703c71a51288c7b71443226548a48
GuibuAdrian <guibuadrian@gmail.com>
2023-06-09 11:03:30 -0300

    Initial commit

---
+++ cmd/web/main.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+	// connect to the database
+
+	// create sessions
+
+	// create loggers
+
+	// create channels
+
+	// create waitgroup
+
+	// set up the application config
+
+	// set up mail
+
+	// listen for web connections
+}
