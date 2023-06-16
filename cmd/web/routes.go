5b2698b3d5a50f68a0acac9dc8966cdd7fd136cd
GuibuAdrian <guibuadrian@gmail.com>
2023-06-16 09:33:39 -0300

    Added Test email sender

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -25,5 +25,25 @@ func (app *Config) routes() http.Handler {
     mux.Post("/register", app.PostRegisterPage)
     mux.Get("/activate-account", app.ActivateAccount)
 
+    mux.Get("/test-mail", app.ActivateAccount) {
+        m := Mail {
+            Domain: "localhost",
+            Host: "localhost",
+            Port: 1025,
+            Encryptation: "none",
+            FromAddress: "info@mycompany.com",
+            FromName: "info",
+            ErrorChan: make(chan error),
+        }
+
+        msg := Message {
+            To: "me@here.com",
+            Subject: "Test mail",
+            Data: "Hello, world!",
+        }
+
+        m.sendMail(msg, make(chan error))
+    }
+
     return mux
 }
