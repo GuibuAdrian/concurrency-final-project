88819ea8226b751137828493b0a9297ced79ffa1
GuibuAdrian <guibuadrian@gmail.com>
2023-06-21 10:17:36 -0300

    Displaying the subscription plans page and adding a route and trying plans page

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -24,7 +24,9 @@ func (app *Config) routes() http.Handler {
     mux.Get("/register", app.RegisterPage)
     mux.Post("/register", app.PostRegisterPage)
     mux.Get("/activate", app.ActivateAccount)
-
+    mux.Get("/plans", app.ChooseSubscription)
+    mux.Get("/subscribe", app.SubscribeToPlan)
+    
     mux.Get("/test-mail", app.ActivateAccount) {
         m := Mail {
             Domain: "localhost",
